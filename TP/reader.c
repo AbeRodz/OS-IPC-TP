bd635f5963dbcbaa5c444bf6fcde9d9c37925d0b
abeRodz <abrahamrodz17@unitec.edu>
2024-07-26 22:13:02 -0600

    UPDATE: added comments to reader

--- TP/reader.c
+++ TP/reader.c
@@ -13,11 +13,8 @@ const char *usr_sign_message =  "SIGN:";
 
 FileLogger *logger, *signLogger;
 
-void detect_sign(){
-
-}
-
 int main(void) {
+    // logger instances
     logger = create_file_logger("Log.txt");
     signLogger = create_file_logger("Sign.txt");
     if ((logger == NULL) || (signLogger == NULL)) {
@@ -29,7 +26,7 @@ int main(void) {
     int num, fifo_fd;
 
     logger->base.log_message((BaseLogger *)logger, LOG_INFO,"waiting for writers...");
-
+    // opening fifo
     fifo_fd = open(FIFO_NAME, O_RDONLY);
     if (fifo_fd == -1) {
         logger->base.log_message((BaseLogger *)logger, LOG_ERROR,"error while opening fifo reader");
@@ -37,31 +34,33 @@ int main(void) {
         return -1;
     }
     logger->base.log_message((BaseLogger *)logger, LOG_INFO,"got a writer");
-
+    // listener loop
     while(1){
+    //reading fifo
+        if ((num = read(fifo_fd, buffer, sizeof(buffer))) == -1){
+            logger->base.log_message((BaseLogger *)logger, LOG_ERROR,"error while reading");
 
-    if ((num = read(fifo_fd, buffer, sizeof(buffer))) == -1){
-        logger->base.log_message((BaseLogger *)logger, LOG_ERROR,"error while reading");
-
-          return -1;
-    }
-    else if (num > 0){
-        buffer[num] = '\0';
-        if (strncmp(buffer, data_message, 5)==0){
-            logger->base.log_message((BaseLogger *)logger, LOG_INFO,"data reader: %s", buffer);
-            logger->base.log_message((BaseLogger *)logger, LOG_INFO,"reader: read %d bytes", num);
-            }
-
-        
-        else if (strncmp(buffer, usr_sign_message,5)==0){
-            signLogger->base.log_message((BaseLogger *)signLogger, LOG_INFO,"signal reader: %s", buffer);
-            }
-            
-        else {
-            logger->base.log_message((BaseLogger *)logger, LOG_ERROR,"received undefined message : %s", buffer);
-            }
+            return -1;
         }
+        
+        else if (num > 0){
+            buffer[num] = '\0';
+            // DATA 
+            if (strncmp(buffer, data_message, 5)==0){
+                logger->base.log_message((BaseLogger *)logger, LOG_INFO,"data reader: %s", buffer);
+                logger->base.log_message((BaseLogger *)logger, LOG_INFO,"reader: read %d bytes", num);
+                }
+            // SIGNAL 
+            else if (strncmp(buffer, usr_sign_message,5)==0){
+                signLogger->base.log_message((BaseLogger *)signLogger, LOG_INFO,"signal reader: %s", buffer);
+                }
+            // undefined or unknown message
+            else {
+                logger->base.log_message((BaseLogger *)logger, LOG_ERROR,"received undefined message : %s", buffer);
+                }
+            }
     } 
+    // closing loggers and fifo
     close_file_logger(logger);
     close_file_logger(signLogger);
     close(fifo_fd);
